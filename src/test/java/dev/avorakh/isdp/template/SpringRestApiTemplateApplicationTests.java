23ed9ead08fee257ca3cb9b9ffb6299e9e8c751e
Aliaksandr Vorakh <39852199+avorakh@users.noreply.github.com>
2025-11-14 00:38:53 +0100

    ci(ISDP-2): init project (#1)

---
+++ src/test/java/dev/avorakh/isdp/template/SpringRestApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.avorakh.isdp.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
